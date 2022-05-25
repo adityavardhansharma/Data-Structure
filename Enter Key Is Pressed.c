7ae41f5f2704e6bda6f984c6aad25d858ac92507
adityavardhansharma <89000612+adityavardhansharma@users.noreply.github.com>
2022-05-25 20:42:19 +0530

    Update Enter Key Is Pressed.c

--- Enter Key Is Pressed.c
+++ Enter Key Is Pressed.c
@@ -1,22 +1,17 @@
 #include <stdio.h>
 int main () 
-
 {
 char c[1000];
-while (c)
-{
 printf(" Start typing\n");
 scanf("%s", c);
-      
+while (c)
+{
 if (getchar () == 0X0A)
 {
 puts ("User pressed Enter");
 break;
 }
 }
-  
 return 0;
-
 }
-
 
